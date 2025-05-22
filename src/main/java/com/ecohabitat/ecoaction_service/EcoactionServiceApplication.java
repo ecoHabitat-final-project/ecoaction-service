08fbace1fb627e9354017ffa9bf581d9acf9c97f
peterParker79 <trypeterparker@gmail.com>
2025-05-22 17:42:29 +0200

    initial commit on port 8084

---
+++ src/main/java/com/ecohabitat/ecoaction_service/EcoactionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ecohabitat.ecoaction_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoactionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoactionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecohabitat/ecoaction_service/EcoactionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecohabitat.ecoaction_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoactionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
